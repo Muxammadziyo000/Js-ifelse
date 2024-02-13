59aa047eba0f8343d260a797c89df7ddbd913c79
Muxammadziyo000 <maximjonsen@gmail.com>
2024-02-13 14:46:41 +0500

    Js-else-if-1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var yosh = prompt("your age:")
+
+if (yosh > 18){
+    console.log("brooooooo");
+}else if(yosh == 18){
+    console.log("adash");
+}else if(yosh << 18){
+    console.log("biratishka");
+}
